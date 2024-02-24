e95a3b25bae6be4afa43ce7840da575712eb4a19
konstask11 <111982386+konstask11@users.noreply.github.com>
2024-02-24 20:57:09 +0200

    Add files via upload

---
+++ src/controllers/user-controller.mjs
@@ -0,0 +1,81 @@
+import bcrypt from 'bcryptjs'
+import {
+  deleteUserById,
+  insertUser,
+  listAllUsers,
+  selectUserById,
+  updateUserById,
+} from '../models/user-model.mjs';
+
+
+const getUsers = async (req, res) => {
+  const result = await listAllUsers();
+  if (result.error) {
+    return res.status(result.error).json(result);
+  }
+  return res.json(result);
+};
+
+const getUserById = async (req, res) => {
+  const result = await selectUserById(req.params.id);
+  if (result.error) {
+    return res.status(result.error).json(result);
+  }
+  return res.json(result);
+};
+
+const postUser = async (req, res) => {
+  const {username, password, email} = req.body;
+  // check that all needed fields are included in request
+  if (username && password && email) {
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+    const result = await insertUser({
+      username,
+      email,
+      password: hashedPassword
+    });
+    if (result.error) {
+      return res.status(result.error).json(result);
+    }
+    return res.status(201).json(result);
+  } else {
+    return res.status(400).json({error: 400, message: 'bad request'});
+  }
+};
+// only user authenticated by token can update or modify own data
+const putUser = async (req, res) => {
+  //get userinfo from req.user object extracted from token
+  const user_id = req.user.user_id;
+  const {username, password, email} = req.body;
+  //hash password if included in request
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(password, salt);
+
+  // check that all needed fields are included in request
+  if (user_id && username && password && email) {
+    const result = await updateUserById({
+      user_id,
+      username,
+      password: hashedPassword,
+      email,
+
+    });
+    if (result.error) {
+      return res.status(result.error).json(result);
+    }
+    return res.status(201).json(result);
+  } else {
+    return res.status(400).json({error: 400, message: 'bad request'});
+  }
+};
+
+const deleteUser = async (req, res) => {
+  const result = await deleteUserById(req.params.id);
+  if (result.error) {
+    return res.status(result.error).json(result);
+  }
+  return res.json(result);
+};
+
+export {getUsers, getUserById, postUser, putUser, deleteUser};
