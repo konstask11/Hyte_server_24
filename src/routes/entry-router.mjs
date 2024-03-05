a08bd853b89f5821239a702547f7a105066f406c
konstask11 <konstask@metropolia.fi>
2024-03-05 11:17:33 +0200

    päivitys

--- src/routes/entry-router.mjs
+++ src/routes/entry-router.mjs
@@ -7,15 +7,27 @@ import {
   deleteEntry,
 } from '../controllers/entry-controller.mjs';
 import {authenticateToken} from '../middlewares/authentication.mjs';
+import { body } from 'express-validator';
 
-// eslint-disable-next-line new-cap
 const entryRouter = express.Router();
 
-entryRouter.route('/')
-    .get(authenticateToken, getEntries).post(authenticateToken, postEntry);
+entryRouter.route('/').get(authenticateToken, getEntries).post(
+body('entry_date').isISO8601(),
+body('mood').notEmpty().isString(),
+body('weight').notEmpty().isNumeric(),
+body('sleep_hours').notEmpty().isFloat({ min: 0, max: 24 }).withMessage('Sleep hours must be between 0h and 24h.'),
+body('notes').optional().isString(),
+authenticateToken,
+postEntry);
 
-entryRouter.route('/:id').get(authenticateToken, getEntryById)
-    .put(authenticateToken, putEntry)
-    .delete(authenticateToken, deleteEntry);
+entryRouter.route('/:id')
+  .get(getEntryById)
+  .put(body('entry_date').optional().isISO8601(),
+  body('mood').optional().isString(),
+  body('weight').optional().isNumeric(),
+  body('sleep_hours').optional().isFloat({ min: 0, max: 24 }).withMessage('Sleep hours must be between 0h and 24h'),
+  body('notes').optional().isString(),
+  authenticateToken, putEntry)
+  .delete(authenticateToken, deleteEntry);
 
 export default entryRouter;
