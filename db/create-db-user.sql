abca2c85400c644d5942b070204420a4d688fd49
konstask11 <konstask@metropolia.fi>
2024-03-12 16:21:29 +0200

    väli commit

--- db/create-db-user.sql
+++ db/create-db-user.sql
@@ -2,3 +2,6 @@ CREATE USER 'healthuser'@'localhost' IDENTIFIED BY 'healthpassu';
 GRANT ALL PRIVILEGES ON `HealthDiary`.* TO 'healthuser'@'localhost';
 FLUSH PRIVILEGES;
 
+CREATE USER 'healthUser'@'localhost' IDENTIFIED BY 'healthPassu';
+GRANT ALL PRIVILEGES ON `HealthDiary`.* TO 'healthUser'@'localhost';
+FLUSH PRIVILEGES;
--- db/health-diary.sql
+++ db/health-diary.sql
@@ -67,16 +67,19 @@ INSERT INTO Users (username, password, email, user_level) VALUES
 INSERT INTO DiaryEntries (user_id, entry_date, mood, weight, sleep_hours, notes, created_at) VALUES
   (1, '2024-01-10', 'Happy', 70.5, 8, 'Had a great day, felt energetic', '2024-01-10 20:00:00'),
   (1, '2024-01-11', 'Tired', 70.2, 6, 'Long day at work, need rest', '2024-01-11 20:00:00'),
-  (2, '2024-01-10', 'Stressed', 65.0, 7, 'Busy day, a bit stressed out', '2023-01-10 21:00:00');
+  (2, '2024-01-10', 'Stressed', 65.0, 7, 'Busy day, a bit stressed out', '2023-01-10 21:00:00'),
+  (5, '2024-03-07', 'Stressed', 124.5, 8, 'Busy day, a bit stressed out', '2024-03-07 21:00:00');
 
 -- Inserting multiple exercise entries
 INSERT INTO Exercises (user_id, exercise_date, exercise_type, duration_minutes, calories_burned, notes, created_at) VALUES
   (1, '2024-01-10', 'Running', 30, 300, 'Morning jog', '2024-01-10 12:00:00'),
   (1, '2024-01-11', 'Weightlifting', 45, 200, 'Strength training', '2024-01-11 15:30:00'),
-  (2, '2024-01-10', 'Yoga', 60, 150, 'Relaxing session', '2023-01-10 18:00:00');
+  (2, '2024-01-10', 'Yoga', 60, 150, 'Relaxing session', '2023-01-10 18:00:00'),
+  (5, '2024-03-06', 'disc golf', 120, 600, 'one round of disc golf', '2023-10-12 15:30:00');
 
 -- Inserting multiple food consumption entries
 INSERT INTO FoodConsumption (user_id, consumption_date, food_name, calories, protein_g, carbohydrates_g, fat_g, notes, created_at) VALUES
   (1, '2024-01-10', 'Salmon and potatoes', 450, 30.0, 20.0, 15.0, 'Healthy dinner', '2024-01-10 19:00:00'),
   (1, '2024-01-11', 'steak with fries', 750, 40.5, 30.0, 25.0, 'Heavy lunch option', '2024-01-11 12:30:00'),
-  (2, '2024-01-10', 'noodle soup', 400, 15.0, 25.0, 10.0, 'Delicious and semi light ', '2024-01-10 20:00:00');
+  (2, '2024-01-10', 'noodle soup', 400, 15.0, 25.0, 10.0, 'Delicious and semi light ', '2024-01-10 20:00:00'),
+  (5, '2024-03-07', 'pasta', 400, 20.0, 35.0, 15.0, 'Healthy dinner ', '2024-03-07 12:00:00');
